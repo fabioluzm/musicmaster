9ca607b5f87cbeff059fa1905d6720e4a1f25eb1
fabioluzm <fabioluzm@gmail.com>
2020-02-27 20:55:08 +0000

    Updates

---
+++ precache-manifest.ce5b8eec00e3b1c1f67804ffbf823702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c0813856e2ac5985b90c100e2461eb",
+    "url": "/musicmaster/index.html"
+  },
+  {
+    "revision": "25e6bb85eac18bfaaff5",
+    "url": "/musicmaster/static/css/main.fe15b74b.chunk.css"
+  },
+  {
+    "revision": "4d6c08a7352122633cb0",
+    "url": "/musicmaster/static/js/2.d8b45f10.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/musicmaster/static/js/2.d8b45f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e6bb85eac18bfaaff5",
+    "url": "/musicmaster/static/js/main.f55fe70b.chunk.js"
+  },
+  {
+    "revision": "bcc3aff62c30317896bb",
+    "url": "/musicmaster/static/js/runtime-main.2c640342.js"
+  }
+]);
