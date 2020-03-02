f78f5a738bed75c0e3a6efedc9da559382723625
fabioluzm <fabioluzm@gmail.com>
2020-03-02 13:34:30 +0000

    spotify auth

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "./app.css";
-import { authEndpoint, clientId, redirectUri, responseType, scopes, showDialog } from "./config";
-import hash from "./hash";
+// import { authEndpoint, clientId, redirectUri, responseType, scopes, showDialog } from "./config";
+// import hash from "./hash";
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 import { GoSearch } from "react-icons/go";
 
@@ -11,59 +11,70 @@ class App extends Component {
         super(props);
 
         this.state = {
-            token: null,
             query: '',
         }
     }
 
-    componentDidMount() {
-        // Set token
-        let _token = hash.access_token;
+    // componentDidMount() {
+    //     // Set token
+    //     let _token = hash.access_token;
     
-        if (_token) {
-          // Set token
-          this.setState({
-            token: _token
-          });
-          this.getSearchQuery(_token);
-        }
-      }
+    //     if (_token) {
+    //       // Set token
+    //       this.setState({
+    //         token: _token
+    //       });
+    //       this.getSearchQuery(_token);
+    //     }
+    //   }
 
+    // searchArtist() {
+    //     console.log('this.state', this.state);
+       
+    //     const AUTHORIZE_URL = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&show_dialog=${showDialog}`;            
+    //     window.location = AUTHORIZE_URL;
+    // }
+
+    // getSearchQuery() {
+    //     const BASE_URL = 'https://api.spotify.com/v1/search?';
+    //     const FETCH_URL = `${BASE_URL}q='${this.state.query}&type=artist&limit=1`;
+    //     // console.log('FETCH_URL', FETCH_URL);
+
+    //     let token = 'Bearer' + _token;
+
+    //     fetch(FETCH_URL, { 
+    //         method: 'GET',
+    //         headers: {
+    //             // 'Authorization': 'Bearer BQAymjIoIR0G9BieGEDaSSBZXDXBjDAgo6ttsxUZL2H__VrmJEBvewVc79CwI8oxHpYJHQ1j3ol6OUG76wjomRlje4mlEjATAa6QUx2ACNrcz5Ko4zE2NmF-8EtISfcBJS0sTDrY-4sxLrRBycrAcK9MMIEgT2VoHO4'
+    //             'Authorizathion': token
+    //         }
+    //     })
+    //     .then(response => response.json())
+    //     .then(json => console.log('json', json));
+    // }
+    
     searchArtist() {
         console.log('this.state', this.state);
-       
-        // const AUTHORIZE_URL = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=${responseType}&show_dialog=${showDialog}`;
-        const AUTHORIZE_URL = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&show_dialog=${showDialog}`;
-            
-        window.location = AUTHORIZE_URL;
-    }
-
-    getSearchQuery(_token) {
-        const BASE_URL = 'https://api.spotify.com/v1/search?';
+         
+        const BASE_URL='https://api.spotify.com/v1/search?';
+        // const FETCH_URL =BASE_URL+'q='+this.state.query+'&type=artist&limit=1';
         const FETCH_URL = `${BASE_URL}q='${this.state.query}&type=artist&limit=1`;
-        // console.log('FETCH_URL', FETCH_URL);
+        // const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+        const auth_token = 'Bearer BQAANJzzkA9S4wGwd_h1nE4kbrx75SDsTsG2k5LdoL65ktLfnefJUMzH_rK4AJmZ1v9Lf3dLb5eNbibNNf2pIf1PcVQV9-aTZ7Vvjn5_5h2DmL5g0ZUZqrNz-n87IoOsHltvpNxHO--BsKTEoEWfjpzXODz6A_vnsDYnNSxzfV_KSySI7DbcOMyhpz539CPnqZQ4hWPX-EKEwkQ_4Oul09yCqb5Y7INDWbfYLJwn8XA8WNtJ-hWvdfQX9tzuv4g2o557BdO6GlPRYMybYv6mzzkhDDnH8Ba0Og';
 
-        let token = 'Bearer' + _token;
 
-        fetch(FETCH_URL, { 
-            method: 'GET',
+        fetch(FETCH_URL,{
+            method:'GET',
             headers: {
-                'Authorization': 'Bearer BQBjvuYP3nKsaUI_KGLwdPtzym5FtKqphLtBeIA-MGhZRU8V8ql3Q-kPfLgsPvbAGjS7fdziS699d6H5t_cJvYBQ3QFzqSEGzbZRoX81LBtMNfgaOJeiu-BMT0cZv_RmgORispo9E20_EFaTdzJp3XJ55iuQQCz2cb0', 
-            }
+            'Content-Type' :'application/json',
+            'Authorization': auth_token,
+            },
+            // mode: 'cors',
+            // cache:'default'
         })
         .then(response => response.json())
         .then(json => console.log('json', json));
-    }
-    // componentDidMount () {
-    //     let _token = hash.access_token;
-        
-    //     if(_token) {
-    //         this.setState({
-    //             token: _token
-    //         });
-    //     }
-    // }    
-        
+    }   
 
     render() {
         return (
--- src/config.jsx
+++ src/config.jsx
@@ -2,7 +2,19 @@ export const authEndpoint = "https://accounts.spotify.com/authorize?";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "96fddf30b44b417e8427c597040731cc";
-export const redirectUri = "http://localhost:3000/callback";
+
+// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+//     export const redirectUri = "http://localhost:3000/callback";
+// } else {
+//     export const redirectUri = "http://localhost:3000/callback";
+// }
+export const LOCAL_DOMAINS = ["localhost", "127.0.0.1"];
+LOCAL_DOMAINS.includes(window.location.hostname) ? alert(window.location.hostname) : alert(window.location.hostname);
+
+let redirectUrl = LOCAL_DOMAINS.includes(window.location.hostname) ? "http://localhost:3000/musicmaster/callback" : `${window.location.hostname}/musicmaster/callback`;
+export const redirectUri = redirectUrl;
+
+// export const redirectUri = "http://localhost:3000/callback";
 export const responseType = "token";
 export const showDialog = "false";
 export const scopes = [
