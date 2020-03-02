ddd26efe0d2dbc24da3eb76b1e241e36ffe56e33
fabioluzm <fabioluzm@gmail.com>
2020-03-02 14:28:27 +0000

    dependecies and spotify json retrieval

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./app.css";
 // import { authEndpoint, clientId, redirectUri, responseType, scopes, showDialog } from "./config";
+import { AUTH_TOKEN } from "./config";
 // import hash from "./hash";
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 import { GoSearch } from "react-icons/go";
@@ -15,59 +16,19 @@ class App extends Component {
         }
     }
 
-    // componentDidMount() {
-    //     // Set token
-    //     let _token = hash.access_token;
-    
-    //     if (_token) {
-    //       // Set token
-    //       this.setState({
-    //         token: _token
-    //       });
-    //       this.getSearchQuery(_token);
-    //     }
-    //   }
-
-    // searchArtist() {
-    //     console.log('this.state', this.state);
-       
-    //     const AUTHORIZE_URL = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&show_dialog=${showDialog}`;            
-    //     window.location = AUTHORIZE_URL;
-    // }
-
-    // getSearchQuery() {
-    //     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    //     const FETCH_URL = `${BASE_URL}q='${this.state.query}&type=artist&limit=1`;
-    //     // console.log('FETCH_URL', FETCH_URL);
-
-    //     let token = 'Bearer' + _token;
-
-    //     fetch(FETCH_URL, { 
-    //         method: 'GET',
-    //         headers: {
-    //             // 'Authorization': 'Bearer BQAymjIoIR0G9BieGEDaSSBZXDXBjDAgo6ttsxUZL2H__VrmJEBvewVc79CwI8oxHpYJHQ1j3ol6OUG76wjomRlje4mlEjATAa6QUx2ACNrcz5Ko4zE2NmF-8EtISfcBJS0sTDrY-4sxLrRBycrAcK9MMIEgT2VoHO4'
-    //             'Authorizathion': token
-    //         }
-    //     })
-    //     .then(response => response.json())
-    //     .then(json => console.log('json', json));
-    // }
     
     searchArtist() {
         console.log('this.state', this.state);
          
         const BASE_URL='https://api.spotify.com/v1/search?';
-        // const FETCH_URL =BASE_URL+'q='+this.state.query+'&type=artist&limit=1';
         const FETCH_URL = `${BASE_URL}q='${this.state.query}&type=artist&limit=1`;
-        // const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-        const auth_token = 'Bearer BQAANJzzkA9S4wGwd_h1nE4kbrx75SDsTsG2k5LdoL65ktLfnefJUMzH_rK4AJmZ1v9Lf3dLb5eNbibNNf2pIf1PcVQV9-aTZ7Vvjn5_5h2DmL5g0ZUZqrNz-n87IoOsHltvpNxHO--BsKTEoEWfjpzXODz6A_vnsDYnNSxzfV_KSySI7DbcOMyhpz539CPnqZQ4hWPX-EKEwkQ_4Oul09yCqb5Y7INDWbfYLJwn8XA8WNtJ-hWvdfQX9tzuv4g2o557BdO6GlPRYMybYv6mzzkhDDnH8Ba0Og';
-
+        // // const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
 
         fetch(FETCH_URL,{
             method:'GET',
             headers: {
             'Content-Type' :'application/json',
-            'Authorization': auth_token,
+            'Authorization': AUTH_TOKEN,
             },
             // mode: 'cors',
             // cache:'default'
--- src/config.jsx
+++ src/config.jsx
@@ -1,24 +1,29 @@
-export const authEndpoint = "https://accounts.spotify.com/authorize?";
+// export const authEndpoint = "https://accounts.spotify.com/authorize?";
 
-// Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "96fddf30b44b417e8427c597040731cc";
+// // Replace with your app's client ID, redirect URI and desired scopes
+// export const clientId = "96fddf30b44b417e8427c597040731cc";
 
-// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-//     export const redirectUri = "http://localhost:3000/callback";
-// } else {
-//     export const redirectUri = "http://localhost:3000/callback";
-// }
-export const LOCAL_DOMAINS = ["localhost", "127.0.0.1"];
-LOCAL_DOMAINS.includes(window.location.hostname) ? alert(window.location.hostname) : alert(window.location.hostname);
+// // if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+// //     export const redirectUri = "http://localhost:3000/callback";
+// // } else {
+// //     export const redirectUri = "http://localhost:3000/callback";
+// // }
+// export const LOCAL_DOMAINS = ["localhost", "127.0.0.1"];
+// LOCAL_DOMAINS.includes(window.location.hostname) ? alert(window.location.hostname) : alert(window.location.hostname);
 
-let redirectUrl = LOCAL_DOMAINS.includes(window.location.hostname) ? "http://localhost:3000/musicmaster/callback" : `${window.location.hostname}/musicmaster/callback`;
-export const redirectUri = redirectUrl;
+// let redirectUrl = LOCAL_DOMAINS.includes(window.location.hostname) ? "http://localhost:3000/musicmaster/callback" : `${window.location.hostname}/musicmaster/callback`;
+// export const redirectUri = redirectUrl;
 
-// export const redirectUri = "http://localhost:3000/callback";
-export const responseType = "token";
-export const showDialog = "false";
-export const scopes = [
-    "user-top-read",
-    "user-read-currently-playing",
-    "user-read-playback-state",
-];
+// // export const redirectUri = "http://localhost:3000/callback";
+// export const responseType = "token";
+// export const showDialog = "false";
+// export const scopes = [
+//     "user-top-read",
+//     "user-read-currently-playing",
+//     "user-read-playback-state",
+// ];
+
+export const TOKEN = 'BQA-x_WO9f986p7wn5BrbJGChE4QqBuVbh5iaqgauyfwjX6P27X2yYeBeJY5N3CWeheENaLcZpzjU8J2GG8pkdinOH6NFzhdBXprxa5yiKO0isVKQXz5IKKRa6C7_x2eFV5J7CTRTRYgde7X0P68b6VRhIhf9KBdEIRIylvWva4bN0HDCO8LbYzonoaKG0d7GNOqNBwDPz9mVEht';
+export const REFRESH_TOKEN = 'AQAlIuF03grT4SbMDAkLRBjjTsaI37dWK4U9wQf4bKg3d_iO3thRgoOiOHCR6LabfSANHP9bjh4LO5oimbREF4Z0Vs2qP_XQBzoMvgxSsMG7jfMyPYLTfeE3c8Hcim1Ti8I'
+export const AUTH_TOKEN = `Bearer ${TOKEN}&refresh_token=${REFRESH_TOKEN}`;
+// export const auth_token = 'Bearer BQA-x_WO9f986p7wn5BrbJGChE4QqBuVbh5iaqgauyfwjX6P27X2yYeBeJY5N3CWeheENaLcZpzjU8J2GG8pkdinOH6NFzhdBXprxa5yiKO0isVKQXz5IKKRa6C7_x2eFV5J7CTRTRYgde7X0P68b6VRhIhf9KBdEIRIylvWva4bN0HDCO8LbYzonoaKG0d7GNOqNBwDPz9mVEht&refresh_token=AQAlIuF03grT4SbMDAkLRBjjTsaI37dWK4U9wQf4bKg3d_iO3thRgoOiOHCR6LabfSANHP9bjh4LO5oimbREF4Z0Vs2qP_XQBzoMvgxSsMG7jfMyPYLTfeE3c8Hcim1Ti8I';
